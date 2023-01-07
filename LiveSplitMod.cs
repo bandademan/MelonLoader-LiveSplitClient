2f223581374ff978fd495a08d5b5c20ca10b51ab
bandademan <aydlettm@yahoo.com>
2023-01-07 01:14:18 -0500

    Setup LiveSplitClient
    Setup the LiveSplit Client and the LiveSplit Mod class

---
+++ LiveSplitMod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.IO;
+using Il2Cpp;
+namespace LiveSplitMod 
+{
+    /*
+     * LiveSplitMod - NameSpace used along side the LiveSplitClient to send commands to the server
+     * 
+     * Place what ever you need within here to start, split, reset etc. for the game you want to mod 
+     */
+    public class LiveSplitMod : MelonMod
+    {
+        private static LiveSplitClient? lsm;
+
+        public override void OnInitializeMelon()
+        {
+            if (lsm == null)
+            {
+                lsm = new LiveSplitClient();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MelonLoader-LiveSplitClient
+## MelonLoader-LiveSplitClient
  
